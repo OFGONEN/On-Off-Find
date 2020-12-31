87fac18aa5e0c993e5962cbc1fd1ff7eeabb4fe5
OFGONEN <ofgonen@gmail.com>
2020-12-31 23:43:56 +0300

    Implement UISpriteImage
    Always same size as the sprite its renders

---
+++ Assets/Scripts/UISpriteImage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FFStudio;
+using UnityEngine.UI;
+
+public class UISpriteImage : UIEntity
+{
+    [SerializeField]
+    private Image imageRenderer;
+    public void SetSprite(Sprite sprite)
+    {
+        uiTransform.sizeDelta = new Vector2(sprite.textureRect.width, sprite.textureRect.height);
+        imageRenderer.sprite = sprite;
+    }
+}
