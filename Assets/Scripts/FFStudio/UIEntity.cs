d36e755693ea79616c63d4842dc9ffacb6d09e72
OFGONEN <ofgonen@gmail.com>
2021-01-01 17:08:12 +0300

    Create UIObjects

--- Assets/Scripts/FFStudio/UIEntity.cs
+++ Assets/Scripts/FFStudio/UIEntity.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using NaughtyAttributes;
 
 namespace FFStudio
 {
@@ -10,15 +11,18 @@ namespace FFStudio
         public CustomGameSettings gameSettings;
         public RectTransform uiTransform;
         public RectTransform destinationTransform;
+        [HideInInspector]
         public Vector3 startPosition;
         public virtual void Start()
         {
             startPosition = uiTransform.position;
         }
+        [Button]
         public virtual void GoTargetPosition()
         {
             uiTransform.DOMove(destinationTransform.position, gameSettings.uiEntityTweenDuration);
         }
+        [Button]
         public virtual void GoStartPosition()
         {
             uiTransform.DOMove(startPosition, gameSettings.uiEntityTweenDuration);
