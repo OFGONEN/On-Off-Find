68d6d3fadea1ce0ddcc941fab0115b6d4d032429
OFGONEN <ofgonen@gmail.com>
2021-01-01 17:39:48 +0300

    Implement Sprite Album

---
+++ Assets/Scripts/UISpriteAlbum.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "SpriteAlbum", menuName = "FF/Data/SpriteAlbum")]
+public class UISpriteAlbum : ScriptableObject
+{
+    public List<Sprite> spriteList;
+    int index;
+
+    public Sprite GiveSprite()
+    {
+        index = index % spriteList.Count;
+
+        var _returnSprite = spriteList[index];
+        index++;
+
+        return _returnSprite;
+    }
+}
