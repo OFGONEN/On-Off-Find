70a8b3535a8c2c3ac10725416e0baa8bcc1f7ede
ofgonen <ofgonen@gmail.com>
2021-01-04 14:31:09 +0300

    Implement Pop Up effect

--- Assets/Scripts/DisappearingEntity.cs
+++ Assets/Scripts/DisappearingEntity.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class DisappearingEntity : Entity
 {
@@ -34,6 +35,7 @@ public class DisappearingEntity : Entity
     }
     public void Disappear()
     {
+        transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
         gameObject.SetActive(false);
         disappearedEntitySet.AddDictionary(entityName, this);
     }
@@ -42,6 +44,20 @@ public class DisappearingEntity : Entity
         gameObject.SetActive(true);
         disappearedEntitySet.RemoveDictionary(entityName);
 
+        var _big = new Vector3(1.15f, 1.15f, 1.15f);
+        var _mid = new Vector3(0.75f, 0.75f, 0.75f);
+
+        var _squence = DOTween.Sequence();
+
+        _squence.Append(transform.DOScale(_big, 0.5f));
+        _squence.Append(transform.DOScale(_mid, 0.125f));
+        _squence.Append(transform.DOScale(Vector3.one, 0.125f));
+
+        _squence.AppendCallback(PlayRightPlaceFX);
+    }
+
+    void PlayRightPlaceFX()
+    {
         rightPlaceFX.gameObject.SetActive(true);
         rightPlaceFX.particles.Play();
     }
